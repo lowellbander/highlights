cb04dd27d0d40e612760f8b994b0f29e8f821163
Lowell Bander <lowellbander@gmail.com>
2017-02-25 14:01:00 -0800

    scrapes and gens json db

--- HighlightsUtil.js
+++ HighlightsUtil.js
@@ -6,7 +6,7 @@ const scrapeIt = require('scrape-it');
 
 const {log} = console;
 
-const contents = fs.readFileSync('tinyHighlights.htm', {encoding: 'utf8'});
+const contents = fs.readFileSync('biggerHighlights.htm', {encoding: 'utf8'});
 
 let $ = cheerio.load(contents);
 
@@ -32,6 +32,46 @@ const parsed = scrapeIt.scrapeHTML($, {
   },
 });
 
-log(parsed);
+const highlights = parsed.highlights;
+const bucketSizes = parsed.meta.map(_ => _.nHighlights);
+const bucketed = bucketHighlights(highlights, bucketSizes);
 
+const database = createDatabase(bucketed, parsed.meta);
+
+fs.writeFileSync('database.json', JSON.stringify(database, null, '\t'));
+
+function createDatabase(bucketed, meta) {
+  if (bucketed.length !== meta.length) {
+    throw 'bucketed and meta must have same length';
+  }
+
+  let database = [];
+  for (let i = 0; i < bucketed.length; ++i) {
+    const title = meta[i].title;
+    const author = meta[i].author;
+    bucketed[i].forEach(quote => {
+      const entry = {
+        quote,
+        title, 
+        author,
+      };
+      database.push(entry);
+    });
+  }
+  return database;
+}
+
+function bucketHighlights(highlights, bucketSizes) {
+  let buckets = [];
+  bucketSizes.forEach(size => buckets.push(unshiftN(highlights, size)));
+  return buckets;
+}
+
+function unshiftN(arr, n) {
+  let result = [];
+  for (let i = 0; i < n; ++i) {
+    result.push(arr.shift());
+  }
+  return result;
+}
 
