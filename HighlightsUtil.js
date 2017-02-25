110bf6ce6fcb7df7816bffd44c8124b67289a4df
Lowell Bander <lowellbander@gmail.com>
2017-02-25 13:19:42 -0800

    real basic scraper

--- HighlightsUtil.js
+++ HighlightsUtil.js
@@ -4,12 +4,34 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const scrapeIt = require('scrape-it');
 
-const contents = fs.readFileSync('tinyHighlights.htm', {encoding: 'ascii'});
-
-// const log = _ => console.log();
-
 const {log} = console;
 
-log(contents);
+const contents = fs.readFileSync('tinyHighlights.htm', {encoding: 'utf8'});
+
+let $ = cheerio.load(contents);
+
+const parsed = scrapeIt.scrapeHTML($, {
+  highlights: {
+    listItem: '.highlight',
+  },
+  meta: {
+    listItem: '.bookMain',
+    data: {
+      title: {
+        selector: '.title',
+      },
+      author: {
+        selector: '.author',
+        convert: _ => _.split(' ').slice(1).join(' '),
+      },
+      nHighlights: {
+        selector: 'span[class*=highlightCount]',
+        convert: _ => parseInt(_),
+      },
+    },
+  },
+});
+
+log(parsed);
 
 
