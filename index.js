3ccaed924bc0652fca7865887105b8bfe2c42553
Lowell Bander <lowellbander@gmail.com>
2017-02-26 18:23:46 -0800

    style changes and small refactors

--- index.js
+++ index.js
@@ -7,84 +7,63 @@ const request = require('request')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
 
-// Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+  res.send('Hello world, I am a chat bot')
 })
 
-// for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
+  if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
 })
 
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text.substring(0, 200)
-            const messageText = getMessageText();
-            sendTextMessage(sender, messageText);
-        }
+  const messaging_events = req.body.entry[0].messaging
+  for (let i = 0; i < messaging_events.length; i++) {
+    const event = req.body.entry[0].messaging[i]
+    const sender = event.sender.id
+    if (event.message) {
+      const messageText = getMessageText();
+      sendTextMessage(sender, messageText);
     }
-    res.sendStatus(200)
+  }
+  res.sendStatus(200)
 })
 
-const token = "EAAQ1j7TjxHUBAGZCJiNp8u5xqai1LZBVZCoRO4IE6RXVR2ba35IUFA2UzaOBcJf7gME3d56LJXy7JtM2LMok0vSWoXQwDuvMHtZA2qBChDoCc8O2uM8WRFibpIVvyosSZCk6DWHchghwE3gfxivkrvBHuPe2ZCAHYU4Eic7MTIoAZDZD";
 
-// Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
 })
 
 function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
-
-// END BOILERPLATE
-
-const PATH = './database.json'
-
-function writeToFile({path, content}) {
-  fs.writeFile(path, content, function(err) {
-    if (err) {
-      return console.log(err);
+  const token = "EAAQ1j7TjxHUBAGZCJiNp8u5xqai1LZBVZCoRO4IE6RXVR2ba35IUFA2UzaOBcJf7gME3d56LJXy7JtM2LMok0vSWoXQwDuvMHtZA2qBChDoCc8O2uM8WRFibpIVvyosSZCk6DWHchghwE3gfxivkrvBHuPe2ZCAHYU4Eic7MTIoAZDZD";
+  const messageData = {text: text};
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData,
+    },
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
     }
-    console.log("The file was saved!");
-  }); 
+  });
 }
 
 function getMessageText() {
-  const database = JSON.parse(fs.readFileSync(PATH));
-  const data = randomElement(database);
-  const _ = '\n\n'
-  return data.quote + _ + data.title + _ + data.author;
+  const path = './database.json'
+  const database = JSON.parse(fs.readFileSync(path));
+  const {quote, title, author} = randomElement(database);
+  return [quote, title, author].join('\n\n');
 }
 
 function randomElement(arr) {
